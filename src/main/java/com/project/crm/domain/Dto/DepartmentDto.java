f309c00cf2c0b7d7fbb520b17f4b906f3b8d0e1f
maklukas <makuch1994@gmail.com>
2021-03-01 18:52:58 +0100

    Dtos has been fetched from bfc915c

---
+++ src/main/java/com/project/crm/domain/Dto/DepartmentDto.java
@@ -0,0 +1,17 @@
+package com.project.crm.domain.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DepartmentDto {
+    private int id;
+    private String name;
+}
---
+++ src/main/java/com/project/crm/domain/Dto/RoleDto.java
@@ -0,0 +1,11 @@
+package com.project.crm.domain.Dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class RoleDto {
+    private int id;
+    private String name;
+}
